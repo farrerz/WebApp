e20c212ef14681e08f32f14520182420b69e2409
BendingUnit-22 <wur1@wit.edu>
2016-11-26 17:44:15 -0500

    index menu

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+
+
+var pingLocalNetwork = function(){
+    alert("Ping local network")
+}
+
+var addItem = function(){
+  alert("add Item")
+}
+
+var showSetting = function(){
+  alert("Setting")
+}
