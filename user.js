8ed0b1539e37d2adc60f49aa004c3884f6382849
BendingUnit-22 <wur1@wit.edu>
2016-11-28 12:45:50 -0500

    added mongodb

---
+++ user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webapp');
+var Schema = mongoose.Schema;
+
+// Scheme
+var userSchema = new Schema({
+  email: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+// Model
+var User = mongoose.model('User', userSchema);
+
+// END
+module.exports = User;
