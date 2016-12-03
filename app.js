5cd0c8cf56bba8f6d9d448507497c39e42153d7d
Rixing <wur1@wit.edu>
2016-12-03 15:09:09 -0500

    schedule task

--- app.js
+++ app.js
@@ -2,52 +2,65 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
+var repeat = require('repeat');
 // Initiala SetUp
-var app       = express();
-var router    = express.Router();
-var port      = process.env.PORT || 3000;
-var fs        = require('fs');
+var app = express();
+var router = express.Router();
+var port = process.env.PORT || 3000;
+var fs = require('fs');
 var publicdir = __dirname + '/public';
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 app.use(session({
-                secret:"adubohv2oenuob23rub2fubb22ef",
-                resave: false,
-                saveUninitialized: true
-              }));
-
+    secret: "adubohv2oenuob23rub2fubb22ef",
+    resave: false,
+    saveUninitialized: true
+}));
 /*--------------------------------------------/
-* Routing GET  *.html
-*--------------------------------------------*/
+ * Repeat Task
+ *--------------------------------------------*/
+
+function runTask() {
+  console.log("Runing tasks..");
+};
+
+repeat(runTask).every(30, 'sec').start.in(1, 'sec');
+/*--------------------------------------------/
+ * Routing GET  *.html
+ *--------------------------------------------*/
 
 
-app.get('/',function(req,res,next){
-    if(!req.session.user){
-      res.redirect('login');
-    }else {
-      next();
+app.get('/', function(req, res, next) {
+    if (!req.session.user) {
+        res.redirect('discover');
+    } else {
+        next();
     }
 });
 
 app.use(function(req, res, next) {
-  if (req.path.indexOf('.') === -1) {
-    var file = publicdir + req.path + '.html';
-    fs.exists(file, function(exists) {
-      if (exists)
-        req.url += '.html';  next();
-    });
-  }else{next();}
+    if (req.path.indexOf('.') === -1) {
+        var file = publicdir + req.path + '.html';
+        fs.exists(file, function(exists) {
+            if (exists)
+                req.url += '.html';
+            next();
+        });
+    } else {
+        next();
+    }
 });
 app.use(express.static(publicdir));
 
 /*--------------------------------------------/
-* Routing POST
-*--------------------------------------------*/
+ * Routing POST
+ *--------------------------------------------*/
 require('./process_request.js')(app);
 
-app.listen(port, function () {
-   console.log('Server running..http://localhost:3000');
+app.listen(port, function() {
+    console.log('Server running..http://localhost:3000');
 });
